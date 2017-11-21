4110e7059a18e52cf37168d3d760681d5439114a
Jyun-De <e1044421301@gm.lhu.edu.tw>
2017-11-21 14:19:37 +0800

    C#語言入門詳解08

---
+++ 08/CSharpMethodExample/CSharpMethodExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            double result = Calculator.GetConeVolume(3.0, 4.0);
+            Console.WriteLine(result);
+        }
+    }
+
+    class Calculator
+    {
+        //https://imgur.com/WLZzA17
+        //https://imgur.com/xCgzRFh
+        //https://imgur.com/S6SaoGU
+        public static double GetCircleArea(double r)
+        {
+            return Math.PI * r * r;
+        }
+        public static double GetCylinderValume(double r, double h)
+        {
+            return GetCircleArea(r)* h;
+        }
+        public static double GetConeVolume(double r, double h)
+        {
+            return GetCylinderValume(r,h) / 3;
+        }
+    }
+}

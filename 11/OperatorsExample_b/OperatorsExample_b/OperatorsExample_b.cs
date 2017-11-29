fe687546aca7af3b288caf3001f143031e590ffb
Jyun-De <e1044421301@gm.lhu.edu.tw>
2017-11-29 17:06:34 +0800

    C#語言入門詳解11

---
+++ 11/OperatorsExample_b/OperatorsExample_b/OperatorsExample_b.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsExample_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = int.MinValue;
+            //int y = checked(-x);//溢出
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //====================
+            //~按位取反
+            int x = 12345678;
+            int y = ~x;
+
+            string xStr = Convert.ToString(x, 2).PadLeft(32, '0');//用0不足至32位元
+            string yStr = Convert.ToString(y, 2).PadLeft(32, '0');
+            Console.WriteLine(xStr);
+            Console.WriteLine(yStr);
+
+            int a = int.MinValue;
+            int b = -a;
+            Console.WriteLine(b);
+            string aStr = Convert.ToString(a, 2).PadLeft(32, '0');
+            Console.WriteLine(aStr);
+
+            //==============================
+            bool b1 = true;
+            bool b2 = !b1;
+            Console.WriteLine(b2);
+            //=============================
+            int c = 100;
+            int d = ++c;//先做++再附值
+            
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+           
+        }
+    }
+}

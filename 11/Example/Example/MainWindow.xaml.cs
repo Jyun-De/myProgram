de7a65be3095af458596a3f08c8b103210991b73
Jyun-De <e1044421301@gm.lhu.edu.tw>
2017-11-29 16:45:20 +0800

    C#語言入門詳解05

---
+++ 11/Example/Example/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //delegate 宣告匿名方法，過時的語法
+            //this.myButton.Click += delegate(object sender, RoutedEventArgs e)
+            //{
+            //    this.myTextBox.Text = "Hello,World!";
+            //};
+
+            //======================================
+            this.myButton.Click += (sender, e)=>
+            {
+                this.myTextBox.Text = "Hello,World!";
+            };
+        }
+
+
+    }
+}
